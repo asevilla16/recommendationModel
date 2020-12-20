2e3630dedd7296aaf70b70d2a59386a47e397394
Maria Salinas <mariasalinas713@gmail.com>
2020-12-20 14:35:26 -0600

    data source bind to dropdown list

--- RestaurantsRecommendation/recommendation.aspx.cs
+++ RestaurantsRecommendation/recommendation.aspx.cs
@@ -17,10 +17,11 @@ namespace RestaurantsRecommendation
     {
         List<Restaurant> restaurants = new List<Restaurant>();
         List<string> restaurantNames = new List<string>();
+        Dictionary<string, string> restaurantsDictionary = new Dictionary<string, string>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            Debug.WriteLine(loadRestaurants());
-
+            loadRestaurants();
         }
         List<Restaurant> loadRestaurants ()
         {
@@ -29,6 +30,10 @@ namespace RestaurantsRecommendation
                 restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(sr.ReadToEnd());
             }
             restaurantNames = MapRestaurantNames();
+            foreach (var item in restaurants)
+            {
+                restaurantsDictionary.Add(item.ID, item.Name);
+            }
             return restaurants;
         }
         protected List<string> MapRecommendationResults (string modelResult)
@@ -46,19 +51,23 @@ namespace RestaurantsRecommendation
         }
         protected void Unnamed1_Click(object sender, EventArgs e)
         {
+            ListBox1.Items.Clear();
             var client = new RestClient("https://ussouthcentral.services.azureml.net/workspaces/bb8a08c8c38f416aa3a883951de0ff4f/services/d0924fd4b0434428be9ca81b23a815af/execute?api-version=2.0&details=true");
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
             request.AddHeader("Authorization", "Bearer lfnl7CdyI6C+d2FZv/G9RxlP1as3YTa6RLJCygYKX+A1kkF9FpN2Xs2w7ypscAtCpMbSlBTCfuHBsRFmfS0i0g==");
             request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("application/json", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"Iduser\",\r\n        \"Rating\",\r\n        \"RestauranID\",\r\n        \"date\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + Iduser.Value + "\",\r\n          \""+ Rating.Value +"\",\r\n          \"" + RestauranID.Value + "\",\r\n          \"" + date.Value + "\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
+            request.AddParameter("application/json", "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"Iduser\",\r\n        \"Rating\",\r\n        \"RestauranID\",\r\n        \"date\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + Iduser.Value + "\",\r\n          \""+ Rating.Value +"\",\r\n          \"" + RestauranID.SelectedValue + "\",\r\n          \"" + date.Value + "\"\r\n        ]\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}", ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
             List<string> results = MapRecommendationResults(response.Content.ToString());
             foreach (var item in results)
             {
                 ListBox1.Items.Add(item);
             }
+            RestauranID.DataTextField = "Value";
+            RestauranID.DataValueField = "Key";
+            RestauranID.DataSource = restaurantsDictionary;
+            RestauranID.DataBind();
         }
     }
 }
